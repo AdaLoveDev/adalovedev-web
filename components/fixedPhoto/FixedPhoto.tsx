6985dae1b162b6acddc638a83e1737cd1bcc6ccc
Yodra López <yodralopez@gmail.com>
2020-10-15 12:22:00 +0100

    remove background image and change logo

--- components/fixedPhoto/FixedPhoto.tsx
+++ components/fixedPhoto/FixedPhoto.tsx
@@ -9,7 +9,9 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
 
 const Container = styled.div`
    min-height: 100vh;
-   background-image: linear-gradient(rgba(106, 32, 90, 0.45), rgba(106, 32, 90, 0.80)), url('https://images.unsplash.com/photo-1573495783078-30b34471804b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80');
+   //background-image: linear-gradient(rgba(106, 32, 90, 0.30), rgba(106,32,90,0.30)), url('/collage.png');
+   //background-image: linear-gradient(rgba(0,0,0,0.45), rgba(0,0,0,0.45)), url('/collage.png');
+   background-image: linear-gradient(rgba(106, 32, 90, 0.60), rgba(106,32,90,0.75));
    background-position: center;
    background-size: cover;
    text-shadow: 0 0 4px rgba(0,0,0,0.5);
@@ -26,15 +28,15 @@ const SocialMedia = styled.div`
 `;
 
 const A = styled.a`
-  color: var(--yellow-color);
+  color: var(--blue-color);
 `;
 
 export const FixedPhoto: FC = () => {
   return <Container>
-    <Logo urlImg="/logo_blue.svg" />
+    <Logo urlImg="/logo_blue_white.svg" />
     <JustifyText>
       <p>Somos una comunidad sin ánimo de lucro cuyo objetivo es dar visibilidad y empoderamiento a las mujeres en el sector tecnológico, concretamente, en el sector de desarrollo de software.</p>
-      <p>Nuestro nombre proviene de un juego de palabras, donde queríamos hacer tributo a Ada Lovelace, considerada la persona que escribió el primer programa de ordenador, junto con la palabra desarrollo (development).</p>
+      <p>Nuestro es un tributo a Ada Lovelace, considerada la persona que escribió el primer programa de ordenador, y el amor por el desarrollo (development).</p>
       <p>Si estás interesada en formar parte de la comunidad, o simplemente quieres colaborar, puedes contactar con nosotras por correo electrónico en <A href="mailto:organization@adalovedev.es">organization@adalovedev.es</A></p>
     </JustifyText>
     <SocialMedia>
